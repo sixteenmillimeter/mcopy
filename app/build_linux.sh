4fb06761aa010c99788196f45ced6a21acead1e5
mmcwilliams <hi@mmcwilliams.com>
2018-01-09 12:35:50 -0500

    Use local binaries, not global ones, in build scripts

--- app/build_linux.sh
+++ app/build_linux.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-electron-packager . mcopy --overwrite --asar=true --platform=linux --arch=x64 --icon=assets/icons/icon.png --prune=true --out=../dist
+#package app
+./node_modules/.bin/electron-packager . mcopy --overwrite --asar=true --platform=linux --arch=x64 --icon=assets/icons/icon.png --prune=true --out=../dist
+#build a .deb installer
+./node_modules/.bin/electron-installer-debian --src ../dist/mcopy-linux-x64/ --dest ../dist/installers/ --arch amd64
