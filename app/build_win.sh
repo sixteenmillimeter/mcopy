f78622c73977b5634c77558d2c206630235b6107
M McWilliams <hi@mmcwilliams.com>
2018-01-30 09:33:16 -0500

    Run build win after building for windows

--- app/build_win.sh
+++ app/build_win.sh
@@ -3,3 +3,5 @@
 ./node_modules/.bin/electron-packager . mcopy --overwrite --asar=true --platform=win32 --arch=x64 --icon=assets/icons/win/icon.ico --prune=true --out=../dist --version-string.CompanyName="sixteenmillimeter.com" --version-string.FileDescription="Open Source Optical Printer Platform" --version-string.ProductName="mcopy"
 
 mkdir ../dist/installers
+
+node ./build_win.js
