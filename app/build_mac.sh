9a45192b38c382398de79b39807d4e9778d9e348
mmcwilliams <hi@mmcwilliams.com>
2018-03-02 21:12:06 -0500

    Sleep after packaging

--- app/build_mac.sh
+++ app/build_mac.sh
@@ -2,8 +2,10 @@
 
 ./node_modules/.bin/electron-packager . --overwrite --platform=darwin --arch=x64 --icon=assets/icons/icon.icns --prune=true --out=../dist
 #build dmg for mac install
+sleep 5s
 mkdir ../dist/installers
-./node_modules/.bin/electron-installer-dmg ../dist/mcopy-darwin-x64 mcopy --out=../dist/installers  --icon=assets/icons/icon.icns        Path to the icon file that will be the app icon in the DMG window.
+./node_modules/.bin/electron-installer-dmg ../dist/mcopy-darwin-x64 mcopy --out=../dist/installers  --icon=assets/icons/icon.icns  --overwrite    
+# Path to the icon file that will be the app icon in the DMG window.
 #  --icon-size=<px>     How big to make the icon for the app in the DMG. [Default: `80`].
 #  --background=<path>  Path to a PNG image to use as the background of the DMG.
 #--overwrite          Overwrite any existing DMG.
