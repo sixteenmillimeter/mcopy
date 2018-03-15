c128fa28710eb286d2aabf23dd86f2b751d12a66
mmcw-dev <hi@mmcwilliams.com>
2018-03-15 14:52:08 -0400

    Improve linux build process. Caught an issue where the binary mcopy is conflicting with another in the mtools library. Will be re-naming the binary mcopy-app on linux. Will revist this after current release.

--- app/build_linux.sh
+++ app/build_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #package app
-./node_modules/.bin/electron-packager . mcopy --overwrite --asar=true --platform=linux --arch=x64 --icon=assets/icons/icon.png --prune=true --out=../dist
+./node_modules/.bin/electron-packager . mcopy --overwrite --asar --platform=linux --arch=x64 --icon=assets/icons/icon.png --prune=true --out=../dist
 #build a .deb installer
-./node_modules/.bin/electron-installer-debian --src ../dist/mcopy-linux-x64/ --dest ../dist/installers/ --arch amd64
+./node_modules/.bin/electron-installer-debian --src ../dist/mcopy-linux-x64/ --arch amd64 --config build_linux.json
