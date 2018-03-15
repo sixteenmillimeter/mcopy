6d6218101c392329dd15c475d1c47fb04c7e3e88
mmcwilliams <hi@mmcwilliams.com>
2018-03-14 23:43:42 -0400

    Update build mac script

--- app/build_mac.sh
+++ app/build_mac.sh
@@ -4,7 +4,7 @@
 #build dmg for mac install
 sleep 5s
 mkdir ../dist/installers
-./node_modules/.bin/electron-installer-dmg ../dist/mcopy-darwin-x64 mcopy --out=../dist/installers  --icon=assets/icons/icon.icns  --overwrite    
+./node_modules/.bin/electron-installer-dmg ../dist/mcopy-darwin-x64/mcopy.app mcopy --out=../dist/installers  --icon=assets/icons/icon.icns  --overwrite    
 # Path to the icon file that will be the app icon in the DMG window.
 #  --icon-size=<px>     How big to make the icon for the app in the DMG. [Default: `80`].
 #  --background=<path>  Path to a PNG image to use as the background of the DMG.
