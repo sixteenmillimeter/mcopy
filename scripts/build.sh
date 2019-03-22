f09ada2cd2b2abd4495194525241725930b263a7
mmcwilliams <hi@mmcwilliams.com>
2019-03-22 19:52:21 -0400

    Build and version the package json files on each npm run build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
-./node_modules/.bin/tsc -p tsconfig.json
+#./node_modules/.bin/tsc -p tsconfig.json
 
 #electron-build fails when local modules are in parent directory
 #copy them into lib directory
 cp -r ./lib/* ./app/lib/
 cp -r ./lib/* ./cli/lib/
+
+cp ./data/cfg.json ./app/data/
+cp ./data/cfg.json ./cli/data/
+
+
+#version all sub projects and config files
+version=$(jq -r  '.version' ./package.json)
+echo "VERSION: $version"
+
+declare -a fileArr=("./data/cfg.json" "./app/package.json" "./cli/package.json")
+
+for i in "${fileArr[@]}"
+do 
+	tmp=$(mktemp)
+	VERSION="$version" jq '.version = env.VERSION' "$i" > "$tmp" && mv "$tmp" "$i"
+done
