b5fbe0153b6784d2b24c929dceeb42280d497561
mmcwilliams <hi@mmcwilliams.com>
2019-06-14 11:58:35 -0400

    Refactor intval module into Typescript.

--- cli/lib/intval/index.js
+++ cli/lib/intval/index.js
@@ -1,71 +1,68 @@
-'use strict'
-
-const req = require('request')
-
+'use strict';
 class Intval {
-	constructor (url) {
-		this._baseUrl = `http://${url}`
-	}
-	async move () {
-		return new Promise ((resolve, reject) => {
-			const timeStart = +new Date()
-			const url = `${this._baseUrl}/frame`
-			//console.log(url)
-			return req(url, (err, res, body) => {
-				let ms = (+new Date()) - timeStart
-				if (err) {
-					return reject(err)
-				}
-				return resolve(ms)
-			})
-		})
-	}
-	async setDir (dir) {
-		return new Promise ((resolve, reject) => {
-			const timeStart = +new Date()
-			const url = `${this._baseUrl}/dir?dir=${dir}`
-			//console.log(url)
-			return req(url, (err, res, body) => {
-				let ms = (+new Date()) - timeStart
-				if (err) {
-					return reject(err)
-				}
-				return resolve(ms)
-			})
-		})
-	}
-	async setExposure (exposure, cb) {
-		return new Promise ((resolve, reject) => {
-			const timeStart = +new Date()
-			const url = `${this._baseUrl}/exposure?exposure=${exposure}`
-			//console.log(url)
-			return req(url, (err, res, body) => {
-				let ms = (+new Date()) - timeStart
-				if (err) {
-					return reject(err)
-				}
-				return resolve(ms)
-			})
-		})
-	}
-	connect (cb) {
-		const timeStart = +new Date()
-		const url = `${this._baseUrl}/status`
-		const opts = {
-			method : 'GET',
-			uri : url,
-			timeout: 5000
-		}
-
-		req(opts, (err, res, body) => {
-			let ms = (+new Date()) - timeStart
-			if (err) {
-				//console.error(err)
-				return cb(err, ms)
-			}
-			cb(null, ms, body)
-		})
-	}
+    constructor(url) {
+        this._baseUrl = `http://${url}`;
+        this.req = require('request');
+    }
+    async move() {
+        return new Promise((resolve, reject) => {
+            const timeStart = +new Date();
+            const url = `${this._baseUrl}/frame`;
+            //console.log(url)
+            return this.req(url, (err, res, body) => {
+                let ms = (+new Date()) - timeStart;
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(ms);
+            });
+        });
+    }
+    async setDir(dir) {
+        return new Promise((resolve, reject) => {
+            const timeStart = +new Date();
+            const url = `${this._baseUrl}/dir?dir=${dir}`;
+            //console.log(url)
+            return this.req(url, (err, res, body) => {
+                let ms = (+new Date()) - timeStart;
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(ms);
+            });
+        });
+    }
+    async setExposure(exposure, cb) {
+        return new Promise((resolve, reject) => {
+            const timeStart = +new Date();
+            const url = `${this._baseUrl}/exposure?exposure=${exposure}`;
+            //console.log(url)
+            return this.req(url, (err, res, body) => {
+                let ms = (+new Date()) - timeStart;
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(ms);
+            });
+        });
+    }
+    connect(cb) {
+        const timeStart = +new Date();
+        const url = `${this._baseUrl}/status`;
+        const opts = {
+            method: 'GET',
+            uri: url,
+            timeout: 5000
+        };
+        this.req(opts, (err, res, body) => {
+            let ms = (+new Date()) - timeStart;
+            if (err) {
+                //console.error(err)
+                return cb(err, ms);
+            }
+            cb(null, ms, body);
+        });
+    }
 }
-
-module.exports = Intval
+module.exports.Intval = Intval;
+//# sourceMappingURL=index.js.map
---
+++ src/intval/index.ts
@@ -0,0 +1,72 @@
+'use strict'
+
+class Intval {
+	private _baseUrl : string
+	private req : any
+	constructor (url : string) {
+		this._baseUrl = `http://${url}`
+		this.req = require('request')
+	}
+	public async move () {
+		return new Promise ((resolve : any, reject : any) => {
+			const timeStart : number = +new Date()
+			const url : string = `${this._baseUrl}/frame`
+			//console.log(url)
+			return this.req(url, (err : Error, res : any, body : string) => {
+				let ms : number = (+new Date()) - timeStart
+				if (err) {
+					return reject(err)
+				}
+				return resolve(ms)
+			})
+		})
+	}
+	public async setDir (dir : boolean) {
+		return new Promise ((resolve : any, reject : any) => {
+			const timeStart : number = +new Date()
+			const url : string = `${this._baseUrl}/dir?dir=${dir}`
+			//console.log(url)
+			return this.req(url, (err : Error, res : any, body : string) => {
+				let ms : number = (+new Date()) - timeStart
+				if (err) {
+					return reject(err)
+				}
+				return resolve(ms)
+			})
+		})
+	}
+	public async setExposure (exposure : number, cb : Function) {
+		return new Promise ((resolve : any, reject : any) => {
+			const timeStart : number = +new Date()
+			const url : string = `${this._baseUrl}/exposure?exposure=${exposure}`
+			//console.log(url)
+			return this.req(url, (err : Error, res : any, body : string) => {
+				let ms : number = (+new Date()) - timeStart
+				if (err) {
+					return reject(err)
+				}
+				return resolve(ms)
+			})
+		})
+	}
+	public connect (cb : Function) {
+		const timeStart : number = +new Date()
+		const url : string = `${this._baseUrl}/status`
+		const opts : any = {
+			method : 'GET',
+			uri : url,
+			timeout: 5000
+		}
+
+		this.req(opts, (err : Error, res : any, body : string) => {
+			let ms : number = (+new Date()) - timeStart
+			if (err) {
+				//console.error(err)
+				return cb(err, ms)
+			}
+			cb(null, ms, body)
+		})
+	}
+}
+
+module.exports.Intval = Intval
