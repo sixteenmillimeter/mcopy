1e2b542e4c26c4dcb0187388d38042d7125853a2
mmcwilliams <hi@mmcwilliams.com>
2019-06-18 15:18:08 -0400

    Begin refactoring devices.js and create a new digital.js lib for the film-out features. Maybe think of renaming now, before it hurts.

--- app/scripts/compile.sh
+++ app/scripts/compile.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
 ./node_modules/.bin/tsc ./src/lib/ui/grid.ts --outFile ./lib/ui/grid.js --noImplicitAny --lib ES2017 --lib ES2016 --lib dom -t ES2016
 ./node_modules/.bin/tsc ./src/lib/ui/seq.ts --outFile ./lib/ui/seq.js --noImplicitAny --lib ES2017 --lib ES2016 --lib dom -t ES2016
+./node_modules/.bin/tsc ./src/lib/ui/digital.ts --outFile ./lib/ui/digital.js --noImplicitAny --lib ES2017 --lib ES2016 --lib dom -t ES2016
