ad67e2f010148138ef53e5ae2e82a1fd42438bf3
mmcwilliams <hi@mmcwilliams.com>
2019-06-24 14:31:26 -0400

    Add devices module after refactor

--- app/scripts/compile.sh
+++ app/scripts/compile.sh
@@ -3,3 +3,4 @@
 ./node_modules/.bin/tsc ./src/lib/ui/grid.ts --outFile ./lib/ui/grid.js --noImplicitAny --lib ES2017 --lib ES2016 --lib dom -t ES2016
 ./node_modules/.bin/tsc ./src/lib/ui/seq.ts --outFile ./lib/ui/seq.js --noImplicitAny --lib ES2017 --lib ES2016 --lib dom -t ES2016
 ./node_modules/.bin/tsc ./src/lib/ui/filmout.ts --outFile ./lib/ui/filmout.js --noImplicitAny --lib ES2017 --lib ES2016 --lib dom -t ES2016
+./node_modules/.bin/tsc ./src/lib/ui/devices.ts --outFile ./lib/ui/devices.js --noImplicitAny --lib ES2017 --lib ES2016 --lib dom -t ES2016
