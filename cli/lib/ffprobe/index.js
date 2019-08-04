2ff98a2148e508df857e0b0afd42f3e58213e692
mmcwilliams <hi@mmcwilliams.com>
2019-08-04 18:04:06 -0400

    Refactor ffprobe into a class. Add jsdoc comments to all methods.

--- cli/lib/ffprobe/index.js
+++ cli/lib/ffprobe/index.js
@@ -1,102 +1,121 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
+/** @module FFPROBE **/
 const fs_extra_1 = require("fs-extra");
 const path_1 = require("path");
 const exec_1 = require("exec");
 //const spawn = require('spawn');
 //const exit = require('exit');
-let system = {};
-async function info(video) {
-    let cmd = `ffprobe -v quiet -print_format json -show_format -show_streams "${video}"`;
-    let fileExists;
-    let raw;
-    let json;
-    let vid;
-    try {
-        fileExists = await fs_extra_1.exists(video);
+class FFPROBE {
+    constructor(sys) {
+        this.system = sys;
     }
-    catch (err) {
-        return exit(err, 5);
+    /**
+     * Get info on a video in json format. Use for filmout.
+     *
+     * @param {string} video Path to video
+     *
+     * @returns {object} Video info in an object
+     **/
+    async info(video) {
+        const cmd = `ffprobe -v quiet -print_format json -show_format -show_streams "${video}"`;
+        let fileExists;
+        let raw;
+        let json;
+        let vid; //whether video has stream with video data
+        try {
+            fileExists = await fs_extra_1.exists(video);
+        }
+        catch (err) {
+            return exit(err, 5);
+        }
+        if (!fileExists) {
+            //return exit(`File ${video} does not exist`, 6);
+            console.error(new Error(`File ${video} does not exist`));
+            return false;
+        }
+        try {
+            console.log(cmd);
+            raw = await exec_1.exec(cmd);
+        }
+        catch (err) {
+            //return exit(err, 7);
+            console.error(err);
+            return false;
+        }
+        try {
+            json = JSON.parse(raw.stdout);
+        }
+        catch (err) {
+            return raw.stdout;
+        }
+        if (json && json.streams) {
+            vid = json.streams.find((stream) => {
+                if (stream.width && stream.height)
+                    return stream;
+            });
+        }
+        if (vid) {
+            json.width = vid.width;
+            json.height = vid.height;
+        }
+        return json;
     }
-    if (!fileExists) {
-        //return exit(`File ${video} does not exist`, 6);
-        console.error(new Error(`File ${video} does not exist`));
-        return false;
+    /**
+     * Count the number of frames in the video using one of two methods.
+     * The first uses -select_streams and is very fast. The second uses
+     * -count_frames and is VERY slow.
+     *
+     * @param {string} video Path to video
+     *
+     * @returns {integer} Number of frames in video
+     **/
+    async frames(video) {
+        const ext = path_1.extname(video.toLowerCase());
+        let cmd = `ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1 "${video}"`;
+        let backup_cmd = `ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 "${video}"`;
+        let fileExists;
+        let raw;
+        let frames;
+        try {
+            fileExists = await fs_extra_1.exists(video);
+        }
+        catch (err) {
+            //return exit(err, 5);
+            console.error(err);
+            return false;
+        }
+        if (!fileExists) {
+            //return exit(`File ${video} does not exist`, 6);
+            console.error(new Error(`File ${video} does not exist`));
+            return false;
+        }
+        if (ext === '.mkv') {
+            cmd = backup_cmd;
+        }
+        try {
+            console.log(cmd);
+            raw = await exec_1.exec(cmd);
+        }
+        catch (err) {
+            console.error(err);
+            return false;
+        }
+        try {
+            frames = parseInt(raw.stdout);
+        }
+        catch (err) {
+            return raw.stdout;
+        }
+        return frames;
     }
-    try {
-        console.log(cmd);
-        raw = await exec_1.exec(cmd);
-    }
-    catch (err) {
-        //return exit(err, 7);
-        console.error(err);
-        return false;
-    }
-    try {
-        json = JSON.parse(raw.stdout);
-    }
-    catch (err) {
-        return raw.stdout;
-    }
-    if (json && json.streams) {
-        vid = json.streams.find((stream) => {
-            if (stream.width && stream.height)
-                return stream;
-        });
-    }
-    if (vid) {
-        json.width = vid.width;
-        json.height = vid.height;
-    }
-    return json;
 }
-async function frames(video) {
-    const ext = path_1.extname(video.toLowerCase());
-    let cmd = `ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1 "${video}"`;
-    let backup_cmd = `ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 "${video}"`;
-    let fileExists;
-    let raw;
-    let frames;
-    try {
-        fileExists = await fs_extra_1.exists(video);
-    }
-    catch (err) {
-        //return exit(err, 5);
-        console.error(err);
-        return false;
-    }
-    if (!fileExists) {
-        //return exit(`File ${video} does not exist`, 6);
-        console.error(new Error(`File ${video} does not exist`));
-        return false;
-    }
-    if (ext === '.mkv') {
-        cmd = backup_cmd;
-    }
-    try {
-        console.log(cmd);
-        raw = await exec_1.exec(cmd);
-    }
-    catch (err) {
-        console.error(err);
-        return false;
-    }
-    try {
-        frames = parseInt(raw.stdout);
-    }
-    catch (err) {
-        return raw.stdout;
-    }
-    return frames;
-}
-function map(obj) {
+/*
+function map (obj : any) {
     console.dir(obj);
 }
+*/
 module.exports = (sys) => {
-    system = sys;
-    return {
-        info,
-        frames
-    };
+    return new FFPROBE(sys);
 };
 //# sourceMappingURL=index.js.map
--- src/ffprobe/index.ts
+++ src/ffprobe/index.ts
@@ -1,109 +1,129 @@
 'use strict';
 
+/** @module FFPROBE **/
+
 import { exists } from 'fs-extra';
 import { extname } from 'path';
 import { exec } from 'exec';
 //const spawn = require('spawn');
 //const exit = require('exit');
 
-let system = {};
-
-async function info (video : string) {
-	let cmd = `ffprobe -v quiet -print_format json -show_format -show_streams "${video}"`
-	let fileExists;
-	let raw;
-	let json;
-	let vid;
-
-	try {
-		fileExists = await exists(video);
-	} catch (err) {
-		return exit(err, 5);
+class FFPROBE {
+	private system : any;
+	private log : any;
+	constructor (sys : any) {
+		this.system = sys;
 	}
-	if (!fileExists) {
-		//return exit(`File ${video} does not exist`, 6);
-		console.error(new Error(`File ${video} does not exist`));
-		return false
-	}
-	
-	try {
-		console.log(cmd);
-		raw = await exec(cmd);
-	} catch (err) {
-		//return exit(err, 7);
-		console.error(err);
-		return false
-	}
+	/**
+	 * Get info on a video in json format. Use for filmout.
+	 *
+	 * @param {string} video Path to video
+	 *
+	 * @returns {object} Video info in an object
+	 **/
+	public async info (video : string) {
+		const cmd : string = `ffprobe -v quiet -print_format json -show_format -show_streams "${video}"`
+		let fileExists : boolean;
+		let raw : any;
+		let json : any;
+		let vid : any; //whether video has stream with video data
 
-	try {
-		json = JSON.parse(raw.stdout);
-	} catch (err) {
-		return raw.stdout;
-	}
+		try {
+			fileExists = await exists(video);
+		} catch (err) {
+			return exit(err, 5);
+		}
+		if (!fileExists) {
+			//return exit(`File ${video} does not exist`, 6);
+			console.error(new Error(`File ${video} does not exist`));
+			return false
+		}
+		
+		try {
+			console.log(cmd);
+			raw = await exec(cmd);
+		} catch (err) {
+			//return exit(err, 7);
+			console.error(err);
+			return false
+		}
 
-	if (json && json.streams) {
-		vid = json.streams.find((stream : any) => {
-			if (stream.width && stream.height) return stream;
-		});
-	}
+		try {
+			json = JSON.parse(raw.stdout);
+		} catch (err) {
+			return raw.stdout;
+		}
 
-	if (vid) {
-		json.width = vid.width;
-		json.height = vid.height;
-	}
+		if (json && json.streams) {
+			vid = json.streams.find((stream : any) => {
+				if (stream.width && stream.height) return stream;
+			});
+		}
 
-	return json;
+		if (vid) {
+			json.width = vid.width;
+			json.height = vid.height;
+		}
+
+		return json;
+	}
+	/**
+	 * Count the number of frames in the video using one of two methods.
+	 * The first uses -select_streams and is very fast. The second uses
+	 * -count_frames and is VERY slow.
+	 *
+	 * @param {string} video Path to video
+	 *
+	 * @returns {integer} Number of frames in video
+	 **/
+	public async frames (video : string) {
+		const ext : string = extname(video.toLowerCase());
+		let cmd : string = `ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1 "${video}"`;
+		let backup_cmd : string = `ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 "${video}"`;
+		let fileExists : boolean;
+		let raw : any;
+		let frames : number;
+
+		try {
+			fileExists = await exists(video);
+		} catch (err) {
+			//return exit(err, 5);
+			console.error(err);
+			return false
+		}
+		if (!fileExists) {
+			//return exit(`File ${video} does not exist`, 6);
+			console.error(new Error(`File ${video} does not exist`));
+			return false;
+		}
+		
+		if (ext === '.mkv') {
+			cmd = backup_cmd;
+		}
+		try {
+			console.log(cmd);
+			raw = await exec(cmd);
+		} catch (err) {
+			console.error(err);
+			return false;
+		}
+
+		try {
+			frames = parseInt(raw.stdout);
+		} catch (err) {
+			return raw.stdout;
+		}
+
+		return frames;
+	}
 }
 
-async function frames (video : string) {
-	const ext : string = extname(video.toLowerCase());
-	let cmd = `ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1 "${video}"`;
-	let backup_cmd = `ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 "${video}"`;
-	let fileExists;
-	let raw;
-	let frames;
-
-	try {
-		fileExists = await exists(video);
-	} catch (err) {
-		//return exit(err, 5);
-		console.error(err);
-		return false
-	}
-	if (!fileExists) {
-		//return exit(`File ${video} does not exist`, 6);
-		console.error(new Error(`File ${video} does not exist`));
-		return false;
-	}
-	
-	if (ext === '.mkv') {
-		cmd = backup_cmd;
-	}
-	try {
-		console.log(cmd);
-		raw = await exec(cmd);
-	} catch (err) {
-		console.error(err);
-		return false;
-	}
-
-	try {
-		frames = parseInt(raw.stdout)
-	} catch (err) {
-		return raw.stdout;
-	}
-
-	return frames;
-}
-
+/*
 function map (obj : any) {
 	console.dir(obj);
 }
+*/
 
 module.exports = (sys : any) => {
-	system = sys;
-	return {
-		info,
-		frames
-	}
+	return new FFPROBE(sys);
 }
