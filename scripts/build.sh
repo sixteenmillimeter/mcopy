3fb276f4016b3f5c937239ee19d1cd4a937c91d6
mmcwilliams <hi@mmcwilliams.com>
2019-12-18 14:14:58 -0500

    Automatically create and remove lib

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+mkdir -p lib
+
 ./node_modules/.bin/tsc -p tsconfig.json --extendedDiagnostics
 
 #electron-build fails when local modules are in parent directory
 #copy them into lib directory
 cp -r ./lib/* ./app/lib/
 cp -r ./lib/* ./cli/lib/
+
+rm -r ./lib
 
 cp ./data/cfg.json ./app/data/
 cp ./data/cfg.json ./cli/data/
