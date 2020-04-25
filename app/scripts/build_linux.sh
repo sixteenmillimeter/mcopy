c15cd512ae69f7871f87e8b02a0076d7bc30085e
mmcwilliams <hi@mmcwilliams.com>
2020-04-24 22:17:10 -0400

    The trick on linux is to not build as an asar. Go figure.

--- app/scripts/build_linux.sh
+++ app/scripts/build_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #package app
-./node_modules/.bin/electron-packager . mcopy-app --overwrite --asar --platform=linux --arch=x64 --icon=assets/icons/icon.png --prune=true --out=../dist
+./node_modules/.bin/electron-packager . mcopy-app --overwrite --platform=linux --arch=x64 --icon=assets/icons/icon.png --prune=true --out=../dist
 #build a .deb installer
 ./node_modules/.bin/electron-installer-debian --src ../dist/mcopy-app-linux-x64/ --arch amd64 --config ./scripts/build_linux.json
