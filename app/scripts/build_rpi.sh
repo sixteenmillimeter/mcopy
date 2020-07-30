7e516a59b65257ec75e6486e6fd01234781a5fe4
mmcwilliams <hi@mmcwilliams.com>
2020-07-30 11:04:02 -0400

    Wrong arch again

--- app/scripts/build_rpi.sh
+++ app/scripts/build_rpi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #package app
-./node_modules/.bin/electron-packager . mcopy-app --overwrite --platform=linux --arch=armv71 --icon=assets/icons/icon.png --prune=true --out=../dist
+./node_modules/.bin/electron-packager . mcopy-app --overwrite --platform=linux --arch=armv7l --icon=assets/icons/icon.png --prune=true --out=../dist
 #build a .deb installer
-./node_modules/.bin/electron-installer-debian --src ../dist/mcopy-app-linux-armv71/ --arch armv71 --config ./scripts/build_linux.json
+./node_modules/.bin/electron-installer-debian --src ../dist/mcopy-app-linux-armv7l/ --arch armv7l --config ./scripts/build_linux.json
