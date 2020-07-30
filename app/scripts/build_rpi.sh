948b20feadf389efdcd8186d77e80c825ecee515
mmcwilliams <hi@mmcwilliams.com>
2020-07-30 11:01:57 -0400

    Install dependencies for rpi. Package as armv71 for raspberry pi

--- app/scripts/build_rpi.sh
+++ app/scripts/build_rpi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #package app
-./node_modules/.bin/electron-packager . mcopy-app --overwrite --platform=linux --arch=armhf --icon=assets/icons/icon.png --prune=true --out=../dist
+./node_modules/.bin/electron-packager . mcopy-app --overwrite --platform=linux --arch=armv71 --icon=assets/icons/icon.png --prune=true --out=../dist
 #build a .deb installer
-./node_modules/.bin/electron-installer-debian --src ../dist/mcopy-app-linux-armhf/ --arch armhf --config ./scripts/build_linux.json
+./node_modules/.bin/electron-installer-debian --src ../dist/mcopy-app-linux-armv71/ --arch armv71 --config ./scripts/build_linux.json
