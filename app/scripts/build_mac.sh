4ef318c55e32a85df7ac2ec2a36c627fdfaf8f00
mmcwilliams <hi@mmcwilliams.com>
2021-04-13 14:45:15 -0400

    Rename installer after build process.

--- app/scripts/build_mac.sh
+++ app/scripts/build_mac.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+version=$(jq -r  '.version' ./package.json)
+
 node ./scripts/build_and_sign_mac.js
-#build dmg for mac install
+
 sleep 5s
+
 mkdir -p ../dist/installers
+
 ./node_modules/.bin/electron-installer-dmg ../dist/mcopy-app-darwin-x64/mcopy-app.app mcopy-app --out=../dist/installers  --icon=assets/icons/icon.icns  --overwrite    
-# Path to the icon file that will be the app icon in the DMG window.
-#  --icon-size=<px>     How big to make the icon for the app in the DMG. [Default: `80`].
-#  --background=<path>  Path to a PNG image to use as the background of the DMG.
-#--overwrite          Overwrite any existing DMG.
+
+
+mv "../dist/installers/mcopy-app.dmg" "../dist/installers/mcopy-app_${version}.dmg"
