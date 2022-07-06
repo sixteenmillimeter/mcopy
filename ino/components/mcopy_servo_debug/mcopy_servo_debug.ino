c0ec81c0f9880c56cf3c888105ac2a450020989e
Matthew McWilliams <Matthew.McWilliams@tufts.edu>
2022-07-06 14:42:16 -0400

    Add Servo debug script

---
+++ ino/components/mcopy_servo_debug/mcopy_servo_debug.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+
+const int PIN_SERVO = 9;
+Servo servo;
+
+/*
+----------------------------------------------------
+Servo      -      Arduino
+           -
+Red        -      5V
+Black      -      GND
+Yellow     -      PWM Pin (9 in this example)
+
+Using TowerPro SG-5010 - default 93
+      TowerPro MG-995  - 
+as servos for development
+----------------------------------------------------
+*/
+
+void setup() {
+  Serial.begin(57600);
+  Serial.flush();
+
+  Servo_init();
+}
+
+void loop() {
+  
+}
+
+void Servo_init () {
+  servo.attach(PIN_SERVO);
+  int angle = servo.read();
+  Serial.print("Default angle: ");
+  Serial.println(angle);
+}
