81b68462614a1c54a2cdc26762b23e8d3975abfe
mattmcw <matt@sixteenmillimeter.com>
2023-02-25 22:22:18 -0500

    Only update patch on main

--- scripts/pre_commit.sh
+++ scripts/pre_commit.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-npm run patch
+branch_name=`sh ./scripts/branch_name.sh`
+
+if [[ "${branch_name}" == "main" ]]; then
+	npm run patch
+else
+	echo "no pre-commit on branch ${branch_name}"
+fi
