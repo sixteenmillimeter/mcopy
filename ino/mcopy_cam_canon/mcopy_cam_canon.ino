0e3bc563a7066aa34f59e315d911a245089c84c4
mattmcw <matt@sixteenmillimeter.com>
2023-03-01 13:51:54 -0500

    Update blink functionality. Probably will not work during start?

--- ino/mcopy_cam_canon/mcopy_cam_canon.ino
+++ ino/mcopy_cam_canon/mcopy_cam_canon.ino
@@ -26,14 +26,16 @@
 
 #define SHUTTTER_BTN    14
 #define FOCUS_BTN   12
-#define LED 2
+#define LED 22
 
 void blink();
 bool ledState;
 
 String name_remote = "mcopy";
 CanonBLERemote canon_ble(name_remote);
-TickTwo blinker(blink, 500, 0, MILLIS);
+TickTwo blinker(blink, 500);
+boolean connected = false;
+long last = -1;
 
 void blink(){
     digitalWrite(LED, ledState);
@@ -58,8 +60,13 @@ void setup()
     }
     while(!canon_ble.pair(10));
 
-    blinker.stop();
+    connected = true;
+    blinker.pause();
+    blinker.interval(100);
+
+    digitalWrite(LED, HIGH);
     delay(1000);
+    
     Serial.println("Camera paired");
     Serial.println(canon_ble.getPairedAddressString());
 }
@@ -67,24 +74,23 @@ void setup()
 void loop()
 {
     // Shutter
-    if (digitalRead(SHUTTTER_BTN) == LOW){
-        blinker.pause();
-        Serial.println("Shutter pressed");
+    if (digitalRead(SHUTTTER_BTN) == LOW && last + 1000 < millis()){
         digitalWrite(LED, LOW);
+        blinker.resume();
+        Serial.println("Shutter pressed");
+
         if(!canon_ble.trigger()){
             Serial.println("Trigger Failed");
         }
-        blinker.resume();
-    }
-    // Focus
-    else if (digitalRead(FOCUS_BTN) == LOW){
+    
         blinker.pause();
-        Serial.println("Focus pressed");
-        digitalWrite(LED, LOW);
-        if(!canon_ble.focus()){
-            Serial.println("Focus failed");
-        }
-        blinker.resume();
+        digitalWrite(LED, HIGH);
+        last = millis();
     }
     blinker.update();
+    if (connected && !canon_ble.isConnected()) {
+        connected = false;
+        Serial.println("Disconnected");
+        blinker.interval(500);
+    }
 }
