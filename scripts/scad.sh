6b77b98aadc785493f14dff0d6accb82001b53b5
mattmcw <matt@sixteenmillimeter.com>
2023-03-04 20:00:01 -0500

    Add more files to scad.sh

--- scripts/scad.sh
+++ scripts/scad.sh
@@ -7,6 +7,19 @@ openscadPart () {
 	fi
 }
 
+# arri s parts
+
+#openscadPart "arri_s.scad" "drive_coupling_DC_connector" 	"arri_s_drive_coupling_DC_connector.stl"
+#openscadPart "arri_s.scad" "drive_coupling_DC" 				"arri_s_drive_coupling_DC.stl"
+#openscadPart "arri_s.scad" "animation_motor_DC_cap" 		"arri_s_animation_motor_DC_cap.stl"
+#openscadPart "arri_s.scad" "animation_motor_DC" 			"arri_s_animation_motor_DC.stl"
+#openscadPart "arri_s.scad" "animation_motor" 				"arri_s_animation_motor.stl"
+#openscadPart "arri_s.scad" "animation_motor_cap" 			"arri_s_animation_motor_cap.stl"
+#openscadPart "arri_s.scad" "drive_coupling" 				"arri_s_drive_coupling.stl"
+#openscadPart "arri_s.scad" "bellows_camera_board_adapter" 	"arri_s_bellows_camera_board_adapter.stl"
+#openscadPart "arri_s.scad" "bellows_camera_board" 			"arri_s_bellows_camera_board.stl"
+
+openscadPart "arri_s_mount.scad" "mount" "arri_s_mount.stl"
 
 # mcopy mono 99 projector controller
 
