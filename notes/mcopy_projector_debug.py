95cde2907a8eea69519cd6cdf2b743a0727b52eb
Matthew McWilliams <matt@sixteenmilimeter.com>
2023-07-03 20:33:32 -0400

    Run the debug script

--- notes/mcopy_projector_debug.py
+++ notes/mcopy_projector_debug.py
@@ -5,20 +5,21 @@ import serial.tools.list_ports
 ports = serial.tools.list_ports.comports()
 port = ''
 for p in ports:
-    print p
-    if "Arduino" in p.description:
-        print "This is an Arduino!"
+    print(p)
+    if "Arduino" in p.description or 'ACM0' in p.description:
+        print("This is an Arduino!")
         port = p.device
         break
 
-if p is '':
+if port == '':
     print("Arduino is not connected")
     exit(1)
 
 arduino = serial.Serial(port=port, baudrate=57600, timeout=.05)
 time.sleep(1)
 
-while arduino.in_waiting:  # Or: while ser.inWaiting():
-    print arduino.readline()
+with open('debug.csv', 'w') as file:
+    while True:  # Or: while ser.inWaiting():
+        file.write(arduino.readline().decode())
 
 
