153ef9eceb233da0f1ac0b9a54862639ab66dc0f
mattmcw <matt@sixteenmillimeter.com>
2023-09-04 10:48:54 -0400

    Use fast-csg and parallel with 8 jobs hardcoded

--- scripts/openscadPart.sh
+++ scripts/openscadPart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-openscad --enable=fast-csg -o "./stl/${3}" -D"PART=\"${2}\"" "./scad/${1}"
+openscad --enable=fast-csg --enable=fast-csg-trust-corefinement --export-format=asciistl -o "./stl/${3}" -D"PART=\"${2}\"" "./scad/${1}"
 echo "Compiled ${3} from ${1}"
 if [ -f "./scad/common/c14n_stl.py" ]; then
 	python3 ./scad/common/c14n_stl.py "./stl/${3}"
