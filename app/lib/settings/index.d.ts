8e8fac92d154068dc606a2924965d025cc8d6d17
mattmcw <matt@sixteenmillimeter.com>
2024-05-22 23:36:40 -0400

    Update Settings class to export properly. Replace use of any in Device.

--- app/lib/settings/index.d.ts
+++ app/lib/settings/index.d.ts
@@ -1,1 +1,39 @@
-export {};
+export declare class Settings {
+    private file;
+    private defaultState;
+    state: any;
+    /**
+     *
+     **/
+    constructor();
+    private exists;
+    private freshState;
+    /**
+     *
+     **/
+    private checkDir;
+    /**
+     *
+     **/
+    save(): Promise<void>;
+    /**
+     *
+     **/
+    update(key: string, val: any): void;
+    /**
+     *
+     **/
+    get(key: string): any;
+    /**
+     *
+     **/
+    all(): any;
+    /**
+     *
+     **/
+    restore(): Promise<void>;
+    /**
+     *
+     **/
+    reset(): Promise<void>;
+}
--- app/lib/settings/index.js
+++ app/lib/settings/index.js
@@ -1,14 +1,15 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
-const os = require("os");
-const path = require("path");
-const fs = require("fs-extra");
+exports.Settings = void 0;
+const os_1 = require("os");
+const path_1 = require("path");
+const promises_1 = require("fs/promises");
 class Settings {
     /**
      *
      **/
     constructor() {
-        this.file = path.join(os.homedir(), `/.mcopy/settings.json`);
+        this.file = (0, path_1.join)((0, os_1.homedir)(), `/.mcopy/settings.json`);
         this.defaultState = {
             server: {
                 port: 1111,
@@ -24,6 +25,15 @@ class Settings {
         };
         this.state = this.freshState();
     }
+    async exists(path) {
+        try {
+            await (0, promises_1.access)(path);
+            return true;
+        }
+        catch (_a) {
+            return false;
+        }
+    }
     freshState() {
         return JSON.parse(JSON.stringify(this.defaultState));
     }
@@ -31,11 +41,11 @@ class Settings {
      *
      **/
     async checkDir() {
-        const dir = path.join(os.homedir(), '.mcopy/');
-        const exists = await fs.exists(dir);
+        const dir = (0, path_1.join)((0, os_1.homedir)(), '.mcopy/');
+        const exists = await this.exists(dir);
         if (!exists) {
             try {
-                await fs.mkdir(dir);
+                await (0, promises_1.mkdir)(dir);
             }
             catch (err) {
                 if (err.code === 'EEXIST')
@@ -52,7 +62,7 @@ class Settings {
         const str = JSON.stringify(this.state, null, '\t');
         this.checkDir();
         try {
-            await fs.writeFile(this.file, str, 'utf8');
+            await (0, promises_1.writeFile)(this.file, str, 'utf8');
         }
         catch (err) {
             console.error(err);
@@ -80,12 +90,12 @@ class Settings {
      *
      **/
     async restore() {
-        let exists;
+        let exists = false;
         let str;
         this.checkDir();
-        exists = await fs.exists(this.file);
+        exists = await this.exists(this.file);
         if (exists) {
-            str = await fs.readFile(this.file, 'utf8');
+            str = await (0, promises_1.readFile)(this.file, 'utf8');
             this.state = JSON.parse(str);
             //console.dir(this.state)
         }
@@ -97,10 +107,10 @@ class Settings {
      *
      **/
     async reset() {
-        const exists = await fs.exists(this.file);
+        const exists = await this.exists(this.file);
         if (exists) {
             try {
-                await fs.unlink(this.file);
+                await (0, promises_1.unlink)(this.file);
             }
             catch (err) {
                 console.error(err);
@@ -111,5 +121,6 @@ class Settings {
     }
     ;
 }
+exports.Settings = Settings;
 module.exports = new Settings();
 //# sourceMappingURL=index.js.map
