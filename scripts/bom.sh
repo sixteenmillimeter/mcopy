e6c65650ded7c144ebd12c0cc61ec67d9b5caf0d
mattmcw <matt@sixteenmillimeter.com>
2024-11-05 20:50:42 -0500

    BOM script calculates total costs of rows of parts and entire BOM

--- scripts/bom.sh
+++ scripts/bom.sh
@@ -32,7 +32,7 @@ TOTAL=$(realpath "${TOTAL}")
 PRICES=$(realpath "./hardware/parts/prices.csv")
 MODULE=""
 
-echo "module,c,part,part_id,description" > "${DESTINATION}"
+echo "module,quantity,part,part_id,description" > "${DESTINATION}"
 
 tac "${1}" | while read line; do
     module=$(echo "${line}" | grep 'module')
@@ -41,15 +41,18 @@ tac "${1}" | while read line; do
     fi
     bom=$(echo "${line}" | grep '//' | grep 'BOM' | awk -F'BOM:' '{print $2}'| xargs)
     if [[ "${bom}" != "" ]]; then
-    	COUNT=$(echo "${bom}" | awk -F',' '{print $1}' | xargs)
+    	QUANTITY=$(echo "${bom}" | awk -F',' '{print $1}' | xargs)
     	PART=$(echo "${bom}" | awk -F',' '{print $2}' | xargs)
     	ID=$(echo "${bom}" | awk -F',' '{print $3}' | xargs)
     	DESCRIPTION=$(echo "${bom}" | awk -F',' '{print $4}' | xargs)
-    	echo "[${MODULE}] ${COUNT}x ${PART} (${ID})"
-    	echo "${MODULE},${COUNT},${PART},${ID},${DESCRIPTION}" >> "${DESTINATION}"
+    	echo "[${MODULE}] ${QUANTITY}x ${PART} (${ID})"
+    	echo "${MODULE},${QUANTITY},${PART},${ID},${DESCRIPTION}" >> "${DESTINATION}"
     fi
 done
 
-echo "count,part_id,part,price" > "${TOTAL}"
+echo "quantity,part_id,part,price" > "${TOTAL}"
 sqlite3 :memory: -cmd '.mode csv' -cmd ".import ${DESTINATION} bom" -cmd ".import ${PRICES} prices"\
-  'SELECT SUM(c),part,part_id, SUM(c) * (SELECT prices.price FROM prices WHERE prices.part = bom.part) as price FROM bom GROUP BY part ORDER BY part DESC;' >> "${TOTAL}"
+  'SELECT SUM(quantity),part,part_id, SUM(quantity) * (COALESCE((SELECT prices.price FROM prices WHERE prices.part = bom.part LIMIT 1), 0)) as price FROM bom GROUP BY part ORDER BY part DESC;' >> "${TOTAL}"
+
+sqlite3 :memory: -cmd '.mode csv' -cmd ".import ${TOTAL} bom"\
+  'SELECT SUM(quantity),"N/A","TOTALS", SUM(price) FROM bom;' >> "${TOTAL}"
